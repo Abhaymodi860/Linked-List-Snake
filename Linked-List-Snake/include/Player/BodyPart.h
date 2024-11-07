e4af5415084aada27500d8f3db27879dcbff9fb8
Abhaymodi860 <modiabhay860@gmail.com>
2024-11-07 16:22:58 +0530

    body part done

--- Linked-List-Snake/include/Player/BodyPart.h
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -14,5 +14,30 @@ namespace Player {
 
 		float bodypart_width;
 		float bodypart_height;
+
+		void createBodyPartImage();
+		void initializeBodyPartImage();
+		sf::Vector2f getBodyPartScreenPosition();
+		float getRotationAngle();
+
+		sf::Vector2i getNextPositionUp();
+		sf::Vector2i getNextPositionDown();
+		sf::Vector2i getNextPositionLeft();
+		sf::Vector2i getNextPositionRight();
+
+		void destroy();
+
+	public:
+		BodyPart();
+		~BodyPart();
+
+		void initialize(float width, float height, sf::Vector2i pos, Direction dir);
+		void updatePosition();
+		void render();
+
+		Direction getDirection();
+		void setDirection(Direction new_direction);
+		sf::Vector2i getPosition();
+		sf::Vector2i getNextPosition();
 	};
 }
